1722942f0918c12fd606d575addfd7f7964de763
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-05-28 09:26:22 -0300

    Initial Commit

---
+++ Day_53-Project11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day_53-Project11
+//
+//  Created by Bruno Oliveira on 28/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day_53-Project11/Day_53_Project11App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_53_Project11App.swift
+//  Day_53-Project11
+//
+//  Created by Bruno Oliveira on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_53_Project11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
